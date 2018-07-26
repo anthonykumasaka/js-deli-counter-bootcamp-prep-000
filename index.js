1a5a1aabf8aade5428347045d99cea07e6f1b3a6
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 03:26:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeli = []
+function takeANumber(katzDeliLine, newPersonsName) {
+      katzDeliLine.push(newPersonsName)
+      return "Welcome, " + newPersonsName + ". You are number " + katzDeliLine.length + " in line."; 
+}
+
+function nowServing(line) {
+  if (katzDeliLine.length === 0 ) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var servingCustomer = katzDeliLine[0]
+    katzDeliLine.shift();
+    return "Currently serving " + servingCustomer + "."; 
+  }
+}
+
+function currentLine(katzDeliLine)
