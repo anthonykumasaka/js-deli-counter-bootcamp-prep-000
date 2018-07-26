cd2bc23cbb2809011b4ce1d27df04194d25090f0
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 03:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
 } else { for (var i = 0; i < katzDeliLine.length; i++) {
-  current = current + " " + (i + 1) + ". " + katzDeliLine[i]
+  var current = current + " " + (i + 1) + ". " + katzDeliLine[i]
 }
   return "The line is currently: " + current; 
 }
