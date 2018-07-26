295b35a324a8f4023dd7f6f1b1a637d013e7ecb6
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 03:49:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
 } else { for (var i = 0; i < katzDeliLine.length; i++) {
-  current = current + (i + 1) + ". " + katzDeliLine[i] + ""; 
+  current = current + (i + 1) + ". " + katzDeliLine[i] + " "; 
 }
   return "The line is currently: " + current; 
 }
